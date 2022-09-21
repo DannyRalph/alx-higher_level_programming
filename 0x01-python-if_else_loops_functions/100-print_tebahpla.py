a9fcc093088175b9ef5f2624b243b5e23cc5821c
DannyRalph <danielraphael847@gmail.com>
2022-09-21 00:49:58 -0700

    quick fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for i in range(ord('z'), ord('a') -1, -1):
+for i in range(ord('z'), ord('a') - 1, -1):
     if i % 2 != 0:
         i = i - (ord('a') - ord('A'))
     print("{:c}".format(i), end="")
