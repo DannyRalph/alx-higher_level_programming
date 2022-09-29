8232dad7e43bbae1d9192e28a40997f890152198
DannyRalph <danielraphael847@gmail.com>
2022-09-29 10:24:47 -0700

    complex_delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!usr/bin/python3
+def complex_delete(a_dictionary, value):
+    for i in a_dictionary.copy():
+        if a_dictionary[i] == value:
+            m = i
+            del a_dictionary[m]
+    return (a_dictionary)
