f8b99a7dc66d40a33993f998e32b7980457bb2e4
DannyRalph <danielraphael847@gmail.com>
2022-09-28 21:04:01 -0700

    at last code works

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,8 @@ def best_score(a_dictionary):
     if a_dictionary:
         for i in a_dictionary:
             if max(zip(a_dictionary.values(), a_dictionary.keys()))[1]:
-                maximum = max(zip(a_dictionary.values(), a_dictionary.keys()))[1]
+                maximum = max(
+                        zip(a_dictionary.values(), a_dictionary.keys()))[1]
                 return (maximum)
             else:
                 return (None)
