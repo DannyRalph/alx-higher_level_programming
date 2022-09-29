61106827fb03614d423f50289eb07cdffb41a306
DannyRalph <danielraphael847@gmail.com>
2022-09-28 20:45:18 -0700

    final trial

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,5 +5,5 @@ def best_score(a_dictionary):
             if max(a_dictionary):
                 maximum = max(a_dictionary)
                 return (maximum)
-            elif max(a_dictionary) is None or len(a_dictionary) == 0:
+            else:
                 return (None)
