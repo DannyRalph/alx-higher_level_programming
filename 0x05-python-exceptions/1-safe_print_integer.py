dc32fb21f8f7a1da50ac033a75650a6120e691fa
DannyRalph <danielraphael847@gmail.com>
2022-10-11 02:06:53 -0700

    quick fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,5 +5,5 @@ def safe_print_integer(value):
         if int(value):
             print("{:d}".format(value))
             return (True)
-    except ValueError:
+    except (ValueError, TypeError):
         return (False)
