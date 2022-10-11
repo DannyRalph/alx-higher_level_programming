5c969a319799baad0811d1f591a30e3bf4b218ee
DannyRalph <danielraphael847@gmail.com>
2022-10-11 07:06:22 -0700

    quick fix

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,6 +5,7 @@ def safe_function(fct, *args):
     try:
         if fct:
             return (fct(*args))
-    except (ValueError, TypeError, NameError, IndexError, ZeroDivisionError) as err:
+    except (ValueError, TypeError, NameError, 
+            IndexError, ZeroDivisionError) as err:
         print("Exception:", err, file=sys.stderr)
         return (None)
